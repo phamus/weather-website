763bae260a1000564befd4f8c89a78486be7a11b
Phamus <sphamus@gmail.com>
2019-07-11 10:27:09 +0100

    made changes to the about page

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -9,7 +9,7 @@ const forcast=(latitude,logtitude, callback)=>{
         }else if(body.error){
             callback('invalid location', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently' + body.currently.temperature + ' degress out.The high today is '+ body.daily.data[0].temperatureHigh+' and the Low today is '+ body.daily.data[0].temperatureLow +' There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
